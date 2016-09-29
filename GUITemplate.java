df3aa9448e202d2b7c63d5b51c24ccf5b16633b9
Cody Orantes Flies <codyflies.aacs159@gmail.com>
2016-09-29 09:04:00 -0700

    updated rectangleCorner method to remove if statement that was causing the little squares to not reapeat

--- GUITemplate.java
+++ GUITemplate.java
@@ -24,11 +24,13 @@ public class GUITemplate extends JPanel {
     private void rectangle(int x, int y, int width, int height, Graphics pen){
         if(width > 0 && height > 0){
             if(y < 500){
-                pen.fillRect(x, y, width, height);// creates the basic square
+                //pen.fillRect(x, y, width, height);// creates the basic square
                 rectangleCorner(x, y, width, height, pen);// puts rectangles around the square/// not putting them around every square for somereason though
+                pen.fillRect(x, y, width, height);
                 if(x < 450){
                     
                     rectangle(x + width + width, y, width, height, pen);// recursion
+                    
                 }
                 else if (x >= 450){/// if x == 450 or more then a new line is created
                     x = width;
@@ -45,13 +47,10 @@ public class GUITemplate extends JPanel {
     /
     **/
     private void rectangleCorner(int x, int y, int width, int height, Graphics pen){
-        if(x == width && y == width){
             pen.fillRect(x-(width/2), y-(height/2), width/2, height/2);//top left
             pen.fillRect(x+x, y+y, width/2, height/2);// bottom right
             pen.fillRect(x+x, y-(height/2), width/2, height/2);//top right
             pen.fillRect(x-(height/2), y+y, width/2, height/2);//bottom left
-            
-        }
     
         
         
